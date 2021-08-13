e397ff3e9e0288446bcaef6bf8ac6c9f737e50c2
Michele Santoni <mikysanto.97@gmail.com>
2021-08-13 23:37:27 +0200

    [TECH] modified camera scene; added ui, nickname panel; Refactoring

---
+++ AR Minigames/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour
+{
+    #region Fields
+    
+    [SerializeField] private NicknameHandler connectionUI = null;
+
+    #endregion
+
+    #region UnityCallbacks
+
+
+
+    #endregion
+
+    #region Methods
+
+
+
+    #endregion
+}
